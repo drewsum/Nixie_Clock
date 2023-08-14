865d506c9954f40b7c47f2c4000d04df788f72f4
Drew <drewmaatman@gmail.com>
2023-08-13 20:12:45 -0500

    wrote pin macros, pulled in configuration.h

---
+++ rev_b/sfw/U1001_Firmware/Nixie_Clock.X/build/default/production/main.o.d
@@ -0,0 +1,8 @@
+build/default/production/main.o: main.c main.h configuration.h \
+ pin_macros.h
+
+main.h:
+
+configuration.h:
+
+pin_macros.h:
--- rev_b/sfw/U1001_Firmware/Nixie_Clock.X/main.c
+++ rev_b/sfw/U1001_Firmware/Nixie_Clock.X/main.c
@@ -5,14 +5,62 @@
  * Created on August 13, 2023, 7:25 PM
  */
 
+#include <xc.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#include "main.h"
 
-    return (EXIT_SUCCESS);
+// Core Drivers
+#include "configuration.h"
+//#include "device_control.h"
+//#include "32mz_interrupt_control.h"
+//#include "heartbeat_timer.h"
+//#include "watchdog_timer.h"
+//#include "error_handler.h"
+//#include "prefetch.h"
+//#include "cause_of_reset.h"
+//#include "rtcc.h"
+
+// GPIO
+#include "pin_macros.h"
+// #include "gpio_setup.h"
+
+// Application
+//#include "heartbeat_services.h"
+//#include "power_saving.h"
+//#include "telemetry.h"
+//#include "capacitive_pushbuttons.h"
+//#include "panel_control.h"
+//#include "generic_buffer_fills.h"
+//#include "splash_screen.h"
+//
+//// I2C
+//#include "plib_i2c.h"
+//#include "plib_i2c_master.h"
+//#include "temperature_sensors.h"
+//#include "power_monitors.h"
+//#include "misc_i2c_devices.h"
+//
+//// USB
+//#include "terminal_control.h"
+//#include "uthash.h"
+//#include "usb_uart.h"
+//#include "usb_uart_rx_lookup_table.h"
+//
+//// ADC
+//#include "adc.h"
+//#include "adc_channels.h"
+
+
+void main(void) {
+
+    while(true) {
+        
+        Nop();
+        
+    }
+
 }
 
