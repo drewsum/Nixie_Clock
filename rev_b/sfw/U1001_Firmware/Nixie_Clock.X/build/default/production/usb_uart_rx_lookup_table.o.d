258edb535e6e9cc3b5765dcaa2d6023a8c926035
Drew <drewmaatman@gmail.com>
2023-09-23 12:01:55 -0500

    rebuilt bare metal project

---
+++ rev_b/sfw/U1001_Firmware/Nixie_Clock.X/build/default/production/usb_uart_rx_lookup_table.o.d
@@ -0,0 +1,49 @@
+build/default/production/usb_uart_rx_lookup_table.o: \
+ usb_uart_rx_lookup_table.c usb_uart_rx_lookup_table.h usb_uart.h \
+ uthash.h main.h terminal_control.h device_control.h \
+ 32mz_interrupt_control.h watchdog_timer.h error_handler.h \
+ cause_of_reset.h prefetch.h power_saving.h heartbeat_services.h \
+ heartbeat_timer.h pin_macros.h misc_i2c_devices.h plib_i2c.h \
+ plib_i2c_master.h ds1683_ETC.h ds3231_rtc.h rtcc.h
+
+usb_uart_rx_lookup_table.h:
+
+usb_uart.h:
+
+uthash.h:
+
+main.h:
+
+terminal_control.h:
+
+device_control.h:
+
+32mz_interrupt_control.h:
+
+watchdog_timer.h:
+
+error_handler.h:
+
+cause_of_reset.h:
+
+prefetch.h:
+
+power_saving.h:
+
+heartbeat_services.h:
+
+heartbeat_timer.h:
+
+pin_macros.h:
+
+misc_i2c_devices.h:
+
+plib_i2c.h:
+
+plib_i2c_master.h:
+
+ds1683_ETC.h:
+
+ds3231_rtc.h:
+
+rtcc.h:
