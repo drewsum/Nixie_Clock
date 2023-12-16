dbc96da520893474a629d307e8deb43323285193
Drew <drewmaatman@gmail.com>
2023-12-15 21:16:19 -0600

    updated main.h

--- rev_b/sfw/U1001_Firmware/Nixie_Clock.X/main.h
+++ rev_b/sfw/U1001_Firmware/Nixie_Clock.X/main.h
@@ -19,9 +19,9 @@
 #ifndef _MAIN_H    /* Guard against multiple inclusion */
 #define _MAIN_H
 
-#define FIRMWARE_VERSION_STR        "0.2"
+#define FIRMWARE_VERSION_STR        "0.9"
 #define PLATFORM_REVISION_STR       "B"
-#define PROJECT_DATE_STR            "September 2023"
+#define PROJECT_DATE_STR            "December 2023"
 #define PROJECT_NAME_STR            "Nixie Clock"
 #define PROJECT_REPOSITORY_STR      "https://github.com/drewsum/Nixie_Clock"
 
